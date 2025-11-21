2dbc32d514060e8ef160e3bbf051c98259c61155
andreasedward58 <andreasedward58@gmail.com>
2025-11-21 12:50:00 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -2,19 +2,18 @@ import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 import numpy as np
+import tempfile
+import cv2
 
-st.title("Trash Detection with YOLOv8")
+st.title("Trash Detection with YOLO")
 
-model = YOLO("best.pt")
+model = YOLO("best.torchscript")
 
 uploaded = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
 
 if uploaded:
     img = Image.open(uploaded)
-    img_np = np.array(img)
-
-    results = model(img_np)
-
+    results = model.predict(img)
     result_img = results[0].plot()
 
-    st.image(result_img, channels="BGR")
+    st.image(result_img, caption="Prediction Result")
