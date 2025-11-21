494bd371df6c006139bdf9103cf9eef9a1797727
andreasedward58 <andreasedward58@gmail.com>
2025-11-21 12:58:55 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
+os.environ["QT_QPA_PLATFORM"] = "offscreen"
+os.environ["OPENCV_VIDEOIO_PRIORITY_MSMF"] = "0"
+
 import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 import numpy as np
-import tempfile
 import cv2
 
-st.title("Trash Detection with YOLO")
+st.title("Trash Detection YOLOv8")
 
 model = YOLO("best.torchscript")
 
@@ -13,7 +16,9 @@ uploaded = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
 
 if uploaded:
     img = Image.open(uploaded)
-    results = model.predict(img)
+    img_np = np.array(img)
+
+    results = model(img_np)
     result_img = results[0].plot()
 
-    st.image(result_img, caption="Prediction Result")
+    st.image(result_img, channels="BGR")
